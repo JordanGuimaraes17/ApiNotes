fb7f32ad4914f7a92989d0bfff33d9032e616eda
Jordan Guimarães <jordanggomes@gmail.com>
2023-11-15 12:10:18 -0300

    API uploads

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,25 @@
-const { Router } = require('express')
+const { Router, request } = require('express')
+const multer = require('multer')
+const uploadConfig = require('../configs/upload')
 
 const UserController = require('../controllers/UsersController')
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 
 const usersRoutes = Router()
+const upload = multer(uploadConfig.MULTER)
 
 const userController = new UserController()
 
 usersRoutes.post('/', userController.create)
 usersRoutes.put('/', ensureAuthenticated, userController.update)
+usersRoutes.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  (request, response) => {
+    console.log(request.file.filename)
+    response.json()
+  }
+)
 
 module.exports = usersRoutes
